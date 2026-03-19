95bd8ad3433c9f4936ea894d3ed6a1d6bb9b2aa6
majzoob1313-collab <majzoob1313@gmail.com>
2026-03-19 15:44:19 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const mineflayer = require('mineflayer')
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'Majzoob.aternos.me',
+    port: 45421,
+    username: 'AFK_Bot',
+    version: '1.20.1'
+  })
+
+  bot.on('spawn', () => {
+    console.log('دخل السيرفر ✅')
+
+    // يمنع الضرب نهائي
+    bot.setControlState('attack', false)
+
+    // يقفز كل 30 ثانية
+    setInterval(() => {
+      bot.setControlState('jump', true)
+
+      setTimeout(() => {
+        bot.setControlState('jump', false)
+      }, 500)
+
+    }, 30000)
+  })
+
+  // تأكيد دائم إنه ما يضرب
+  bot.on('physicsTick', () => {
+    if (bot.controlState.attack) {
+      bot.setControlState('attack', false)
+    }
+  })
+
+  bot.on('end', () => {
+    console.log('انقطع... يرجع بعد 5 ثواني 🔄')
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('error', console.log)
+  bot.on('kicked', console.log)
+}
+
+createBot()
